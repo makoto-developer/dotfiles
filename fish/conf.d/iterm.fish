bbcdbb90f45440e4d8a64fdd8b9cbf12cdf2cc6c
makoto-developer <72484465+makoto-developer@users.noreply.github.com>
2023-11-14 19:45:28 +0900

    バックグランド画面の背景色を変える

--- fish/conf.d/iterm.fish
+++ fish/conf.d/iterm.fish
@@ -1,7 +1,24 @@
 # itermをカスタマイズする関数
 
-## バックグランドのカラーを変える
-alias b_prod='echo -ne "\033]1337;SetColors=bg=330000\a"'
-alias b_stg='echo -ne "\033]1337;SetColors=bg=003366\a"'
-alias b_work='echo -ne "\033]1337;SetColors=bg=5B0066\a"'
-alias b_work2='echo -ne "\033]1337;SetColors=bg=00665B\a"'
+function changeBackgroundColor
+  switch $argv
+    case 'prod'
+    case 'prd'
+      echo -ne "\033]1337;SetColors=bg=330000\a"
+    case 'stg'
+      echo -ne "\033]1337;SetColors=bg=003366\a"
+    case 'work'
+      echo -ne "\033]1337;SetColors=bg=5B0066\a"
+    case 'work2'
+      echo -ne "\033]1337;SetColors=bg=00665B\a"
+    case 'default'
+      echo -ne "\033]1337;SetColors=bg=000000\a"
+  end
+end
+
+alias prod='changeBackgroundColor "prod"'
+alias stg='changeBackgroundColor "stg"'
+alias work='changeBackgroundColor "work"'
+alias work2='changeBackgroundColor "work2"'
+alias reset='changeBackgroundColor "default"'
+
